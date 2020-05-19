f76d9a087ccf14f892738cc05906d2b65f563c50
beaub <beaub@microsoft.com>
2020-05-19 13:38:59 -0700

    Use macro pad for chrome tabs

--- keyboards/keebio/quefrency/keymaps/beau/keymap.c
+++ keyboards/keebio/quefrency/keymaps/beau/keymap.c
@@ -17,9 +17,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_60_with_macro(
     G(KC_1), G(KC_2), KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_BSPC, \
     G(KC_3), G(KC_4), KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I,    KC_O,   KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
-    KC_HOME, KC_END,  KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
-    KC_INS,  KC_DEL,  KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RSFT, \
-    _______,  _______,  KC_LCTL, KC_LALT, MO(_FN1), KC_LGUI, KC_SPC,      MO(_FN1),MO(_FN1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
+    C(KC_1), C(KC_2), KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K,    KC_L,   KC_SCLN, KC_QUOT, KC_ENT, \
+    C(KC_3), C(KC_4), KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, KC_RSFT, \
+    C(KC_5), C(KC_6), KC_LCTL, KC_LALT, MO(_FN1), KC_LGUI, KC_SPC,      MO(_FN1),MO(_FN1),  KC_RCTL, KC_LEFT, KC_DOWN, KC_UP, KC_RGHT
   ),
 
   [_FN1] = LAYOUT_60_with_macro(
